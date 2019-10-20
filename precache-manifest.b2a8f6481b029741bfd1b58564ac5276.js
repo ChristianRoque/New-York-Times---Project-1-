53a2223ae41f871d92478610da195d2af4489a73
Christian Roque <croqu008@fiu.edu>
2019-10-19 20:52:12 -0400

    Updates

---
+++ precache-manifest.b2a8f6481b029741bfd1b58564ac5276.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c54e1830d87c9e1ad6999d1352026b1",
+    "url": "/New-York-API/index.html"
+  },
+  {
+    "revision": "dd98ae7208b27e8af971",
+    "url": "/New-York-API/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fc3f948756d4af5fead7",
+    "url": "/New-York-API/static/css/main.7a4b4868.chunk.css"
+  },
+  {
+    "revision": "dd98ae7208b27e8af971",
+    "url": "/New-York-API/static/js/2.596b06c7.chunk.js"
+  },
+  {
+    "revision": "fc3f948756d4af5fead7",
+    "url": "/New-York-API/static/js/main.d7a25e02.chunk.js"
+  },
+  {
+    "revision": "54b611980bef783d9b81",
+    "url": "/New-York-API/static/js/runtime-main.5552ea10.js"
+  },
+  {
+    "revision": "d29046dbf234d77bf57676f0f906c2a8",
+    "url": "/New-York-API/static/media/background.d29046db.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/New-York-API/static/media/logo.25bf045c.svg"
+  }
+]);
